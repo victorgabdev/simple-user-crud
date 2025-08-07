0d287cb0c1017e75bb7832ffd9c73fbff0264c2c
victorgabdev <victorgabdev@gmail.com>
2025-08-07 11:23:09 -0300

    refactor: GlobalExceptionHandÃler class

--- src/main/java/com/victorgabdev/usercrud/exception/GlobalExceptionHandler.java
+++ src/main/java/com/victorgabdev/usercrud/exception/GlobalExceptionHandler.java
@@ -1,8 +1,12 @@
 package com.victorgabdev.usercrud.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.time.LocalDateTime;
 
@@ -10,12 +14,40 @@ import java.time.LocalDateTime;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
-    public ResponseEntity<?> handleBusinessException(BusinessException ex) {
+    public ResponseEntity<?> handleBusinessException(BusinessException ex,  HttpServletRequest request) {
         ApplicationErrors error = new ApplicationErrors(
                 ex.getMessage(),
                 ex.getStatus().value(),
-                LocalDateTime.now());
+                LocalDateTime.now(),
+                request.getRequestURI(),
+                "NOT FOUND");
 
         return new ResponseEntity<>(error, ex.getStatus());
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<?> handleNotFond(NoHandlerFoundException ex) {
+        ApplicationErrors error = new ApplicationErrors(
+                ex.getMessage(),
+                ex.getStatusCode().value(),
+                LocalDateTime.now(),
+                ex.getRequestURL(),
+                "Página ou rota não encontrada."
+        );
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<?> handleNotFond(NoResourceFoundException ex, HttpServletRequest request) {
+        ApplicationErrors error = new ApplicationErrors(
+                ex.getMessage(),
+                HttpStatus.NOT_FOUND.value(),
+                LocalDateTime.now(),
+                request.getRequestURI(),
+                "Recurso não encontrado"
+        );
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
 }
